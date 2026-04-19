fa17aeda431b66482cf3fccc92ab2095db055396
fredpromexia <foutoujosna73@gmail.com>
2026-04-19 20:36:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
